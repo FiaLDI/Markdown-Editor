8b2df04a3de318c0f4ad66110dec620d52a72ca9
FiaLDI <leon.youtube2017@yandex.ru>
2026-06-08 08:03:03 +0300

    feat: add files routes

---
+++ backend/src/api/v1/files/deps/index.ts
@@ -0,0 +1,5 @@
+import { FilesModule } from '@/modules/files';
+
+export const filesV1Imports = [
+  FilesModule,
+];
--- backend/src/api/v1/index.ts
+++ backend/src/api/v1/index.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthV1ApiModule } from './auth';
 import { HealthV1ApiModule } from './health';
+import { FilesV1ApiModule } from './files';
 
 @Module({
   imports: [
     AuthV1ApiModule,
-    HealthV1ApiModule
+    HealthV1ApiModule,
+    FilesV1ApiModule,
   ],
 })
 export class ApiV1Module {}
